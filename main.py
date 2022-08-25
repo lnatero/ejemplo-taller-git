d0ae993d7c83e9ac51c0dd3cd602dff2d2cde501
Lucas <lnatero0@gmail.com>
2022-08-25 17:50:33 -0400

    fix: typo en aleatorio

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random  # importamos libreria random casos 'aleatoreos'
+import random  # importamos libreria random casos 'aleatorios'
 import os  # importamos libreria para interactuar con archivos
 
 print(
@@ -34,10 +34,10 @@ for e in lista_de_fotos:
 input("Presiona ENTER para obtener una foto al azar de perritos ")
 
 # Usamos la funcion choice de random para obtener una foto al azar
-foto_aleatorea = random.choice(lista_de_fotos)
+foto_aleatoria = random.choice(lista_de_fotos)
 
 # Concatenamos para obtener la ruta completa de la foto
-ruta_foto = os.path.join("fotos",foto_aleatorea)
+ruta_foto = os.path.join("fotos",foto_aleatoria)
 
 # Usamos la liberia os para abrir la foto seleccionada
 os.startfile(ruta_foto)
